0f95b5e237c602fa2019a0c13c8299c6f56cbf3c
Manuel Stoeckel <s3676959@stud.uni-frankfurt.de>
2019-10-11 19:32:54 +0200

    Finished Model class
    - Added checkpoint saving and loading.
    - Moved main methods to own file.

---
+++ main.py
@@ -0,0 +1,173 @@
+import glob
+from pathlib import Path
+from typing import Union
+
+import torch
+from torch import nn as nn, optim as optim
+
+from dataset import EosDataset
+from model import DeepEosModel, train, evaluate
+
+
+def train_multi():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    window_size = 6
+    model_name = 'multi.SETIMES2'
+
+    print("Train data")
+    train_data = EosDataset(
+        glob.glob('data/SETIMES2.*.train'), shuffle_input=False, split_dev=False,
+        save_vocab=Path('multi.SETIMES2/').joinpath(model_name + '.vocab'),
+        window_size=window_size, min_freq=10
+    )
+
+    print("Dev data")
+    dev_data = EosDataset(
+        glob.glob('data/SETIMES2.*.dev'), shuffle_input=False, split_dev=False,
+        load_vocab=Path('multi.SETIMES2/').joinpath(model_name + '.vocab'),
+        window_size=window_size
+    )
+
+    print("Test data")
+    test_data = EosDataset(
+        glob.glob('data/SETIMES2.*.test'), shuffle_input=False, split_dev=False,
+        load_vocab=Path('multi.SETIMES2/').joinpath(model_name + '.vocab'),
+        window_size=window_size
+    )
+
+    leipzig_test = EosDataset(
+        'data/deu_mixed-typical_2011_30K-formatted.txt', shuffle_input=True, split_dev=False,
+        load_vocab=Path('multi.SETIMES2/').joinpath(model_name + '.vocab'),
+        window_size=window_size
+    )
+
+    model = DeepEosModel(rnn_bidirectional=True, dropout=0.2)
+    if torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)
+    else:
+        model.to(device)
+    print(model)
+
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    print("Training")
+    model = train(model, train_data, dev_data, batch_size=32, epochs=1,
+                  optimizer=optimizer, device=device, base_path='multi.SETIMES2/')
+
+    print("Testing with SETimes2")
+    evaluate(model, test_data, device=device)
+
+    print("Testing with Leipzig mixed typical")
+    evaluate(model, leipzig_test, device=device)
+
+    return model
+
+
+def train_leipzig():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    window_size = 6
+
+    model_name = 'de.leipzig'
+    leipzig = EosDataset(
+        'data/deu_wikipedia_2016_100K-formatted.txt',
+        split_dev=True, save_vocab=Path('leipzig/').joinpath(model_name + '.vocab'),
+        window_size=window_size, min_freq=10
+    )
+    train_data, dev_data = leipzig.train, leipzig.dev
+
+    model = DeepEosModel(rnn_bidirectional=True, dropout=0.2)
+    if torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)
+    else:
+        model.to(device)
+    print(model)
+
+    # Pre-train on Leipzig corpus
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    train(model, train_data, dev_data, batch_size=32, epochs=3,
+          optimizer=optimizer, device=device, base_path='leipzig/')
+
+    return model
+
+
+def fine_tune(model: Union[DeepEosModel, str], vocab_path: Union[str, Path], cross_validation_set=None):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    window_size = 6
+
+    if type(model) is str:
+        model = torch.load(str(model), map_location=device)
+
+    # BIOfid fine-tuning
+    biofid_train = EosDataset(
+        'data/bioFID_train_cleaned.txt', shuffle_input=False,
+        split_dev=False, load_vocab=vocab_path, window_size=window_size
+    )
+    biofid_test = EosDataset(
+        'data/bioFID_test.txt', shuffle_input=False,
+        split_dev=False, load_vocab=vocab_path, window_size=window_size
+    )
+
+    print("\nBIOfid test scores prior to fine-tuning")
+    evaluate(model, biofid_test, device=device)
+    print(flush=True)
+
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+    train(model, biofid_train, biofid_test, base_path=Path('biofid_models/'),
+          optimizer=optimizer, epochs=10, device=device)
+
+    print("\nBIOfid test")
+    evaluate(model, biofid_test, device=device)
+
+    if cross_validation_set is not None:
+        print("\nCross validation")
+        evaluate(model, cross_validation_set, device=device)
+
+    model_name = 'de.biofid'
+    torch.save(model, Path('biofid_models/').joinpath(model_name + '.h5'))
+
+
+if __name__ == '__main__':
+    # model = train_multi()
+    model = str(Path('multi.SETIMES2').joinpath('best_model.pt'))
+    model = DeepEosModel.load(model).cuda()
+    print(model)
+    vocab_path = Path('multi.SETIMES2/').joinpath('multi.SETIMES2.vocab')
+
+    path = 'data/deu_mixed-typical_2011_30K-formatted.txt'
+    print(path)
+    leipzig_mixed_typical = EosDataset(
+        path, shuffle_input=True, split_dev=False,
+        load_vocab=vocab_path, window_size=6
+    )
+    evaluate(model, leipzig_mixed_typical)
+
+    path = 'data/deu_wikipedia_2016_10K-formatted.txt'
+    print(path)
+    leipzig_wiki = EosDataset(
+        path, shuffle_input=True, split_dev=False,
+        load_vocab=vocab_path, window_size=6
+    )
+    evaluate(model, leipzig_wiki)
+
+    for path in glob.glob('data/SETIMES2.*.test'):
+        print(path)
+        test_data = EosDataset(
+            path, shuffle_input=False, split_dev=False,
+            load_vocab=vocab_path, window_size=6
+        )
+        evaluate(model, test_data)
+
+    for path in glob.glob('data/europarl-v7.*.test'):
+        print(path)
+        test_data = EosDataset(
+            path, shuffle_input=False, split_dev=False,
+            load_vocab=vocab_path, window_size=6
+        )
+        evaluate(model, test_data)
+
+    # test_data = EosDataset(
+    #     glob.glob('data/SETIMES2.*.test'), shuffle_input=False, split_dev=False,
+    #     load_vocab=vocab_path,
+    #     window_size=6
+    # )
+    # fine_tune(model, vocab_path, test_data)
--- model.py
+++ model.py
@@ -1,3 +1,8 @@
+"""
+Model class and functions for the PyTorch implementation of DeepEOS.
+"""
+__author__ = 'Manuel Stoeckel'
+
 from pathlib import Path
 from typing import Union
 
@@ -14,64 +19,122 @@ from util import AverageMeter
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-class DeepEos(nn.Module):
+class DeepEosModel(nn.Module):
     def __init__(self, max_features=1000, embedding_size=128, rnn_type: str = 'LSTM', rnn_size=256,
-                 dropout=.25, rnn_layers=1, rnn_bidirectional=False):
-        super(DeepEos, self).__init__()
+                 dropout=0.2, rnn_layers=1, rnn_bidirectional=False):
+        super(DeepEosModel, self).__init__()
 
-        self.emb = nn.Embedding(num_embeddings=max_features,
-                                embedding_dim=embedding_size)
+        # Hyperparameters
+        self.max_features = max_features
+        self.embdding_size = embedding_size
+        self.rnn_type = rnn_type.lower()
         self.rnn_size = rnn_size
+        self.dropout = dropout
         self.rnn_layers = rnn_layers
-        if rnn_type.lower() == 'lstm':
-            self.rnn = nn.LSTM(input_size=embedding_size,
+        self.rnn_bidirectional = rnn_bidirectional
+
+        # Layers
+        self.emb = nn.Embedding(num_embeddings=self.max_features,
+                                embedding_dim=self.embdding_size)
+
+        if self.rnn_type == 'lstm':
+            self.rnn = nn.LSTM(input_size=self.embdding_size,
                                hidden_size=self.rnn_size,
                                num_layers=self.rnn_layers,
-                               dropout=dropout if self.rnn_layers > 1 else 0.0,
-                               bidirectional=rnn_bidirectional,
+                               dropout=self.dropout if self.rnn_layers > 1 else 0.0,
+                               bidirectional=self.rnn_bidirectional,
                                batch_first=True)
         elif str(rnn_type).lower() == 'gru':
-            self.rnn = nn.GRU(input_size=embedding_size,
+            self.rnn = nn.GRU(input_size=self.embdding_size,
                               hidden_size=self.rnn_size,
                               num_layers=self.rnn_layers,
-                              dropout=dropout if self.rnn_layers > 1 else 0.0,
-                              bidirectional=rnn_bidirectional,
+                              dropout=self.dropout if self.rnn_layers > 1 else 0.0,
+                              bidirectional=self.rnn_bidirectional,
                               batch_first=True)
         else:
             raise NotImplementedError(f"'{rnn_type}' is not a valid RNN type choice. Please use: [LSTM, GRU]\n")
 
-        self.dropout = nn.Dropout(p=dropout)
-        self.dense = nn.Linear(in_features=self.rnn_size * (2 if rnn_bidirectional else 1),
+        self._dropout = nn.Dropout(p=self.dropout)
+        self.dense = nn.Linear(in_features=self.rnn_size * (2 if self.rnn_bidirectional else 1),
                                out_features=1)
 
     def forward(self, input: torch.Tensor):
         output = self.emb(input)
-        output, _ = self.rnn.forward_impl(output, None, None, input.size(0), None)
-        output = self.dropout(output[:, -1, :].squeeze())
+        output, _ = self.rnn(output)
+        output = self._dropout(output[:, -1, :].squeeze())
         output = self.dense(output)
         return torch.sigmoid(output)
 
+    def checkpoint(self, model_path: Union[str, Path]):
+        """
+        Create a checkpoint file a the given path.
 
-def train(model: DeepEos, train_dataset, dev_datset=None, optimizer=None, epochs=5, batch_size=32,
-          evaluate_after_epoch=True, eval_batch_size=32,
-          device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
-    if torch.cuda.device_count() > 1:
-        model = nn.DataParallel(model)
-    model.to(device)
+        :param model_path: The file path for the new checkpoint.
+        :return: None
+        """
+        model_dict = {
+            'hyper_params': {
+                'max_features': self.max_features,
+                'embedding_size': self.embdding_size,
+                'rnn_size': self.rnn_size,
+                'rnn_layers': self.rnn_layers,
+                'rnn_type': self.rnn_type,
+                'dropout': self.dropout,
+                'rnn_bidirectional': self.rnn_bidirectional
+            },
+            'state_dict': self.state_dict()
+        }
+        torch.save(model_dict, str(model_path), pickle_protocol=4)
+
+    @staticmethod
+    def load(model_path: Union[Path, str]):
+        if type(model_path) is str:
+            model_path = Path(model_path)
+
+        model_dict = torch.load(model_path)
+        model = DeepEosModel(**model_dict['hyper_params'])
+        model.load_state_dict(model_dict['state_dict'])
+        return model
+
+
+def train(model: DeepEosModel, train_dataset, dev_dataset=None, optimizer=None, epochs=5, batch_size=32,
+          evaluate_after_epoch=True, eval_batch_size=32, base_path: Union[str, Path] = None, save_checkpoints=True,
+          eval_metric='precision',
+          device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")) -> DeepEosModel:
+    """
+    Train the given DeepEosModel with the given datasets using Binary-Cross-Entropy loss.
+
+    :param model: The model to train.
+    :param train_dataset: The train dataset.
+    :param dev_dataset: The dev dataset.
+    :param optimizer: The optimizer, defaults to Adam with lr=0.001.
+    :param epochs: The number of training epochs.
+    :param batch_size: The batch size.
+    :param evaluate_after_epoch: If true, evaluate after each batch using the dev dataset.
+    :param eval_batch_size: The batch size for the evaluation.
+    :param base_path: The base path for model checkpoints.
+    :param save_checkpoints: If True, save checkpoints after each epoch in the given base path.
+     Will save the last and the best model in separate files.
+    :param eval_metric: The evaluation metric to choose the best model.
+    :param device: See torch.device.
+    :return: The best model if checkpointing and dev evaluation were enabled. The last model otherwise.
+    """
     criterion = nn.BCELoss()
     criterion.to(device)
 
     if optimizer is None:
         optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-    # Training
+    if type(base_path) is str:
+        base_path = Path(base_path)
+    best_score = -1.0
+
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     for epoch in range(epochs):
-        print(f"Epoch {epoch + 1}")
         model.train()
         total_batches = len(train_loader)
         average = AverageMeter()
-        with tqdm(train_loader, total=total_batches, desc="Training") as tq:
+        with tqdm(train_loader, total=total_batches, desc=f"Epoch {epoch + 1}") as tq:
             for batch_no, (y_train, x_train) in enumerate(tq):
                 x_train = x_train.long().to(device)
 
@@ -86,18 +149,44 @@ def train(model: DeepEos, train_dataset, dev_datset=None, optimizer=None, epochs
                 average.update(loss.item())
                 tq.set_postfix_str(f"loss: {average.avg:0.5f} ({loss.item():0.5f})", True)
 
-        # Evaluate
-        if evaluate_after_epoch and dev_datset is not None:
-            evaluate(model, dev_datset, eval_batch_size, device)
+        if evaluate_after_epoch and dev_dataset is not None:
+            score = evaluate(model, dev_dataset, eval_batch_size, metric=eval_metric, device=device)
+
+            if save_checkpoints and base_path is not None and score > best_score:
+                best_score = score
+                model_file = base_path / "best_model.pt"
+                model.checkpoint(model_file)
+
+        if save_checkpoints and base_path is not None:
+            model_file = base_path / "last_model.pt"
+            model.checkpoint(model_file)
+
+    if epochs > 1 and evaluate_after_epoch and dev_dataset is not None and save_checkpoints and base_path is not None:
+        print(
+            "Loading best scoring model\n"
+            f"{eval_metric.title()}: {best_score * 100:0.2f}%"
+        )
+        return DeepEosModel.load(str(base_path / "best_model.pt")).to(device)
+    return model
 
 
-def evaluate(model: DeepEos, dev_datset: Union[EosDataset, list], batch_size=32,
-             device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")):
+def evaluate(model: DeepEosModel, datset: Union[EosDataset, list], batch_size=32, metric='precision',
+             device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")) -> float:
+    """
+    Evaluate the given model.
+
+    :param model: The model to evaluate.
+    :param datset: The evaluation dataset.
+    :param batch_size: The evaluation batch size.
+    :param metric: The evaluation metric to return.
+    :param device: See torch.device.
+    :return:
+    """
     model.eval()
 
     true_samples = []
     pred_samples = []
-    dev_loader = DataLoader(dev_datset, batch_size=batch_size, shuffle=True)
+    dev_loader = DataLoader(datset, batch_size=batch_size, shuffle=True)
     with tqdm(dev_loader, total=len(dev_loader), desc="Evaluating", position=0) as tq:
         for batch_no, (y_eval, x_eval) in enumerate(tq):
             true_samples.extend(y_eval.squeeze().bool().cpu().tolist())
@@ -106,60 +195,15 @@ def evaluate(model: DeepEos, dev_datset: Union[EosDataset, list], batch_size=32,
             prediction = model(x_eval) >= 0.5
             pred_samples.extend(prediction.squeeze().bool().cpu().tolist())
 
-    print(f"Precision: {precision_score(true_samples, pred_samples, pos_label=True) * 100:0.2f}%, "
-          f"Recall: {recall_score(true_samples, pred_samples, pos_label=True) * 100:0.2f}%, "
-          f"F1: {f1_score(true_samples, pred_samples, pos_label=True) * 100:0.2f}%", flush=True)
-
-
-def main():
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    window_size = 6
-
-    model_name = 'de.biofid'
-    leipzig = EosDataset(
-        Path('data/deu_wikipedia_2016_100K-formatted.txt'),
-        split_dev=True, save_vocab=Path('models/').joinpath(model_name + '.vocab'),
-        window_size=window_size, min_freq=10
-    )
-    train_data, dev_data = leipzig.train, leipzig.dev
-    # model_name = 'multi.SETIMES2'
-    # train_data = EosDataset(
-    #     Path('data/SETIMES2.all.train'), shuffle_input=False,
-    #     split_dev=False, save_vocab=Path('models/').joinpath(model_name + '.vocab'),
-    #     window_size=window_size, min_freq=10
-    # )
-    # dev_data = EosDataset(
-    #     Path('data/SETIMES2.all.dev'), shuffle_input=False,
-    #     split_dev=False, load_vocab=Path('models/').joinpath(model_name + '.vocab'),
-    #     window_size=window_size, min_freq=10
-    # )
-    biofid_train = EosDataset(
-        Path('data/bioFID_train_cleaned.txt'), shuffle_input=False,
-        split_dev=False, load_vocab=Path('models/').joinpath(model_name + '.vocab'),
-        window_size=window_size
-    )
-    biofid_test = EosDataset(
-        Path('data/bioFID_test.txt'), shuffle_input=False,
-        split_dev=False, load_vocab=Path('models/').joinpath(model_name + '.vocab'),
-        window_size=window_size
-    )
-
-    model = DeepEos(rnn_bidirectional=True)  # TODO
-    # pre-train
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)
-    train(model, train_data, dev_data, batch_size=32,
-          optimizer=optimizer, epochs=1, device=device)
-    print("\nBIOfid dev")
-    evaluate(model, biofid_test.train, device=device)
-    print(flush=True)
-
-    # BIOfid train
-    # optimizer = optim.Adam(model.parameters(), lr=0.0001)
-    train(model, biofid_train.train, biofid_test.train,
-          optimizer=optimizer, epochs=1, device=device)
-
-    torch.save(model.state_dict(), Path('models/').joinpath('multi.SETIMES2.h5'))
-
-
-if __name__ == '__main__':
-    main()
+    precision = precision_score(true_samples, pred_samples, pos_label=True)
+    recall = recall_score(true_samples, pred_samples, pos_label=True)
+    f1 = f1_score(true_samples, pred_samples, pos_label=True)
+    print(f"Precision: {precision * 100:0.2f}%, "
+          f"Recall: {recall * 100:0.2f}%, "
+          f"F1: {f1 * 100:0.2f}%", flush=True)
+    if metric is 'recall':
+        return recall
+    elif metric is 'f1':
+        return f1
+    else:
+        return precision
